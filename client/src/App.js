de05840a0739677e92f1f5d1f2e86db53e0d0f26
JhoraMormont <ignacio.salinas00@gmail.com>
2021-10-14 19:15:42 -0300

    Client: add react router.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Chat from './Chat';
+import Login from './Login';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Chat socket={this.props.socket} />
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <Login />
+            </Route>
+            <Route path="/chat">
+              <Chat socket={this.props.socket} />
+            </Route>
+          </Switch>
+        </Router>
       </div>
     );
   }
--- client/src/Login/Login.js
+++ client/src/Login/Login.js
@@ -1,7 +1,52 @@
 import React from 'react';
 
 export default class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { password: '', userName: '' };
+
+    this.handleUserNameInputChange = this.handleUserNameInputChange.bind(this);
+    this.handlePasswordInputChange = this.handlePasswordInputChange.bind(this);
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+  }
+
+  handleFormSubmit(event) {
+    event.preventDefault();
+    this.setState({ password: '', userName: '' });
+  }
+
+  handleUserNameInputChange(event) {
+    this.setState({ userName: event.target.value });
+  }
+
+  handlePasswordInputChange(event) {
+    this.setState({ password: event.target.value });
+  }
+
   render() {
-    return <h1>Login Page</h1>;
+    return (
+      <div>
+        <h1>Login Page</h1>
+        <form onSubmit={this.handleFormSubmit}>
+          <label>
+            User name
+            <input
+              type="text"
+              value={this.state.userName}
+              onChange={this.handleUserNameInputChange}
+            />
+          </label>
+          <label>
+            Password
+            <input
+              type="password"
+              value={this.state.password}
+              onChange={this.handlePasswordInputChange}
+            />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
   }
 }
